76d0e0a5665dc49565724c67626c7f52c7e08249
Shengyou Fan <shengyoufan@gmail.com>
2014-03-04 15:08:13 +0800

    write a robo script for update barryvdh/laravel-ide-helper

--- RoboFile.php
+++ RoboFile.php
@@ -2,4 +2,14 @@
 class Robofile extends \Robo\Tasks
 {
     // define public methods as commands
+    public function updateIdehelper()
+    {
+    	$this->say('Update barryvdh/laravel-ide-helper');
+    	$this->taskExecStack('Update barryvdh/laravel-ide-helper')
+ 			->stopOnFail()
+ 			->exec('php artisan clear-compiled')
+ 			->exec('php artisan ide-helper:generate')
+ 			->exec('php artisan optimize')
+ 			->run();
+    }
 }
