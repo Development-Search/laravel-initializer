d6c82b719ddeb6daa1c9748a9dff20c30bc67398
Shengyou Fan <shengyoufan@gmail.com>
2014-03-04 12:48:40 +0800

    change laravel environment setup method
    - change detect environment method from array to closure
    - make start.php to read .environment.php for environment name

---
+++ .environment.sample.php
@@ -0,0 +1,12 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Setup Your Application Environment
+|--------------------------------------------------------------------------
+|
+| Force to setup you application enviroment by: 
+| 'local', 'staging', 'production'
+|
+*/
+return '';
