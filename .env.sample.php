c19f2858b8d954f21777302167dd580bb8e73231
Shengyou Fan <shengyoufan@gmail.com>
2014-03-04 14:11:48 +0800

    tweak a little about .env.sample.php

--- .env.sample.php
+++ .env.sample.php
@@ -1,8 +1,20 @@
 <?php
 
 return array(
+
+	/*
+	|--------------------------------------------------------------------------
+	| Credentials
+	|--------------------------------------------------------------------------
+	|
+	| list your credentials data here
+	|
+	*/
+
+	// MySQL
 	'MYSQL_HOST'     => '',
 	'MYSQL_DATABASE' => '',
 	'MYSQL_USERNAME' => '',
 	'MYSQL_PASSWORD' => '',
+
 );
